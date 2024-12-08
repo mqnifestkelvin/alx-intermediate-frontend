27f6ffc66a91483d1b28bdecb70e961746369ae9
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-12-08 22:00:20 +0100

    done

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}",
+      "./1-index.html",
+      "./2-index.html",
+      "./3-nav_index.html",
+      "./4-flexbox_index.html",
+      "./5-gridflex_index.html",
+      "./6-imageGallery.html"
+    ],
+
     theme: {
       extend: {},
     },
